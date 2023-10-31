36ad908ec3419f826e5d854ac5dcfcd7ea3f729a
AmirE142 <65749316+AmirE142@users.noreply.github.com>
2023-10-30 21:06:41 -0700

    Add files via upload

---
+++ Python TDD/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\Career Related\Skill Building\Coding\Ex_Files_Learning_Python\Python TDD\superlists\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
